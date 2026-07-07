c8f33d3402de89aee39c7c0f413c0c2670ee9794
mariabodnarcuk <mariia.bodnarchuk.25@kse.org.ua>
2026-07-07 22:04:29 +0300

    third task completed

--- task3.py
+++ task3.py
@@ -5,3 +5,17 @@ exam_results = [
 {"student_name": "Григорій", "score": 45}
 ]
 passing_score = 60 # Прохідний бал
+
+# 1. Напишіть цикл, який проходить по кожному словнику у списку
+# exam_results.
+# 2. Всередині циклу порівняйте значення ключа "score" кожного студента зі
+# змінною passing_score.
+# 3. Додайте до кожного словника новий ключ "passed":
+# ● Значення має бути True, якщо бал студента є більшим або рівним
+# прохідному.
+# ● Значення має бути False, якщо бал менший.
+# 4. Виведіть на екран фінальний список exam_results з доданою
+# інформацією про складання іспиту.
+for student in exam_results:
+    student["passed"] = True if student["score"] > passing_score else False
+print(exam_results)
